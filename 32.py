61b40a2b29f3bc2ed37a4dabe9a99c5aba938052
Vanger <stepan13@gmail.com>
2023-02-21 21:30:34 +0000

    initial

---
+++ 32.py
@@ -0,0 +1,18 @@
+# Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону 
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+from random import randint
+
+n = randint(1,20)
+defmax = randint(0,100)
+defmin = randint(0,defmax)
+list = [randint(0,100) for _ in range(n)]
+res = []
+
+for i in range(len(list)):
+    if list[i] >= defmin and list[i] <= defmax:
+        res.append(i)
+
+print(list)
+print(f"min {defmin} max {defmax}")
+print(res)
